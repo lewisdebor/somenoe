b5b0d4b29ce0e41fea4ebc679f471cbfb8bcda9b
lewisdebor <lewisdeborahpozow@gmail.com>
2024-11-30 15:51:39 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://bronsed.co.ua'
+	const proxyUrl = 'https://vokunokiu.xyz'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {

16db7dd6a9a5467f577c583fa44b0528cc327203
lewisdebor <lewisdeborahpozow@gmail.com>
2024-11-30 19:39:25 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://vokunokiu.xyz'
+	const proxyUrl = 'https://bunolilk.buzz'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
